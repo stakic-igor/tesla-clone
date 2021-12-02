efcdda4e9b206115b70c0b8dbbb4890f7f1854cc
Igor Stakic <stakic_igor@yahoo.com>
2021-12-02 12:44:13 +0100

    Add basic page section component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/Header";
+import PageSections from "./components/PageSections";
 
 function App() {
   return (
     <div>
       <Header />
+      <PageSections />
       tesla clone
     </div>
   );
---
+++ src/components/PageSections.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageSections = () => {
+    return (
+        <section>
+            <img src="./images/house.jpg" />
+        </section>
+    )
+}
+
+export default PageSections;
